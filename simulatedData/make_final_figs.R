c939d68d71cde755029b0a12dae6d847361e721b
rokblagus <57137527+rokblagus@users.noreply.github.com>
2025-06-11 13:36:51 +0200

    min

--- simulatedData/make_final_figs.R
+++ simulatedData/make_final_figs.R
@@ -21,49 +21,57 @@ df2$model2<-ifelse(df2$model=="LMM","Linear~Mixed~Model",ifelse(df2$model=="BMM"
 df2$model2<-factor(df2$model2,levels=c("Linear~Mixed~Model","Binomial~Mixed~Model","Poisson~Mixed~Model"))
 df$model2<-factor(df$model2,levels=c("Linear~Mixed~Model","Binomial~Mixed~Model","Poisson~Mixed~Model"))
 
+df$group<-ifelse(df$N==25,"1","2")
+df2$group<-ifelse(df2$N==25,"1","2")
 
+ 
+nms<-c("N=25\n n=\"moderate\"","N=25\n n=\"large\"","N=50\n n=\"moderate\"","N=50\n n=\"large\"")
+nms<-c("N=25\n n=moderate","N=25\n n=large","N=50\n n=moderate","N=50\n n=large")
 
-p0<-ggplot(df[df$rho==0.5,])+
-  geom_point(aes(x=Nn2,y=bound_t*100,colour =  method),size=2)+
-  geom_line(aes(x=Nn2,y=bound_t*100,colour =  method),size=.7)+
+p0<-ggplot(df[df$rho==0.5 ,],aes(color=method,group=interaction(method, group)) )+
+  geom_point(aes(x=Nn2,y=bound_t*100,color=method  ),size=2)+
+  geom_line(aes(x=Nn2,y=bound_t*100,color=method  ),size=.7)+
   facet_grid(model2~vars,labeller = label_parsed)+
   theme_light()+
-  scale_x_continuous(name="", breaks=c(1,2,3,4), labels=label_parsed(levels(factor(df$Nn))))+
+  scale_x_continuous(name="", breaks=c(1,2,3,4), labels=nms,limits = c(0.75,4.25))+
   ylab("boundary estimate (%)")+
-  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))+
+  theme(axis.text.x = element_text( vjust = 0.5, hjust=0.5))+
   geom_hline(yintercept=0) +
   scale_colour_manual(values=dfpallete)+
   theme(legend.title=element_blank())
 
 
-pp<-ggplot(df2[df2$rho==0.5&!(df2$method=="BM"&df2$model=="LMM"),])+
+ 
+
+
+pp<-ggplot(df2[df2$rho==0.5&!(df2$method=="BM"&df2$model=="LMM"),],aes(color=method,group=interaction(method, group)))+
   geom_point(aes(x=Nn2,y=prial,colour =  method),size=2)+
   geom_line(aes(x=Nn2,y=prial,colour =  method),size=0.7)+
   facet_grid(model2~vars,scales="free_y",labeller = label_parsed)+
   theme_light()+
-  scale_x_continuous(name="", breaks=c(1,2,3,4), labels=label_parsed(levels(factor(df2$Nn))))+
+  scale_x_continuous(name="", breaks=c(1,2,3,4), labels=nms,limits = c(0.75,4.25))+
   ylab("PRIAL (%)")+
-  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))+
+  theme(axis.text.x = element_text( vjust = 0.5, hjust=0.5))+
   geom_hline(yintercept=0)+
   scale_colour_manual(values=dfpallete2)+
   theme(legend.title=element_blank())
 
-tiff("Fig_bound_all.tif",height=6*3/2,width=12,units="in",res=1200,compression = "lzw")
+tiff("Fig_bound_all.tif",height=6*3/2*0.8,width=12,units="in",res=1200,compression = "lzw")
 p0
 dev.off()
 
 
-pdf("Fig_bound_all.pdf",height=6*3/2,width=12 )
+pdf("Fig_bound_all.pdf",height=6*3/2*0.8,width=12 )
 p0
 dev.off()
 
 
-tiff("Fig_prial_all.tif",height=6*3/2,width=12,units="in",res=1200,compression = "lzw")
+tiff("Fig_prial_all.tif",height=6*3/2*0.8,width=12,units="in",res=1200,compression = "lzw")
 pp
 dev.off()
 
 
-pdf("Fig_prial_all.pdf",height=6*3/2,width=12 )
+pdf("Fig_prial_all.pdf",height=6*3/2*0.8,width=12 )
 pp
 dev.off()
 
