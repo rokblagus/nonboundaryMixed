30b1f69e718139981803841a12534d5ab1e42c57
rokblagus <57137527+rokblagus@users.noreply.github.com>
2025-08-22 08:02:21 +0200

    min

--- simulatedData/make_scripts.R
+++ simulatedData/make_scripts.R
@@ -81,3 +81,90 @@ for (N in c(25,50)){
         }
       }}}}
 
+
+####aded, avg 25 to address rev concerns
+
+
+####scripts for lmm
+
+  
+zz=32
+for (N in c(25,50)){
+  for (n in c(10,20)){
+    for (vr_int in c(0.001)){
+      for (mult in c(0.1)){
+        for (rh in c(0.5,0.8)){
+          mod="lmm"
+    zz=zz+1        
+      cat(
+        "set.seed(",zz,")\n",
+        "num_cluster =",N,"\n",
+        "num_subj=",n,"\n",
+        "sd_int=sqrt(",vr_int,")\n",
+        "multiplier_int=",mult,"\n",
+        "rho=",rh,"\n",
+        "model=\"",mod,"\"\n",
+        "source(\"../source_fun.R\")"
+        ,file=paste0("lmm/script_",zz,".R"),sep="")
+                  
+            
+          }
+}}}}
+
+
+
+####binom
+
+zz=32
+for (N in c(25,50)){
+  for (n in c(10,20)){
+    for (vr_int in c(0.001)){
+      for (mult in c(0.1)){
+        for (rh in c(0.5,0.8)){
+          mod="bin"
+          zz=zz+1        
+          cat(
+            "set.seed(",zz,")\n",
+            "num_cluster =",N,"\n",
+            "num_subj=",n,"\n",
+            "sd_int=sqrt(",vr_int,")\n",
+            "multiplier_int=",mult,"\n",
+            "rho=",rh,"\n",
+            "model=\"",mod,"\"\n",
+            "source(\"../source_fun.R\")"
+            ,file=paste0("bin/script_",zz,".R"),sep="")
+          
+          
+        }
+      }}}}
+
+
+
+
+####pois
+
+zz=32
+for (N in c(25,50)){
+  for (n in c(10,20)){
+    for (vr_int in c(0.001)){
+      for (mult in c(0.1)){
+        for (rh in c(0.5,0.8)){
+          mod="pois"
+          zz=zz+1        
+          cat(
+            "set.seed(",zz,")\n",
+            "num_cluster =",N,"\n",
+            "num_subj=",n,"\n",
+            "sd_int=sqrt(",vr_int,")\n",
+            "multiplier_int=",mult,"\n",
+            "rho=",rh,"\n",
+            "model=\"",mod,"\"\n",
+            "source(\"../source_fun.R\")"
+            ,file=paste0("pois/script_",zz,".R"),sep="")
+          
+          
+        }
+      }}}}
+
+
+
